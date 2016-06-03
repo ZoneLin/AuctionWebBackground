5da17923fe0308f109313cf13209e3d162fba026
tim84312 <q8952526@gmail.com>
2016-06-03 21:16:09 +0800

    Finsh First AllProduct Class

--- TeamC/AllProductTest.cs
+++ TeamC/AllProductTest.cs
@@ -10,5 +10,54 @@ namespace TeamC
     [TestFixture]
     class AllProductTest
     {
+        [Test]
+        public void TestInsertAndGet()
+        {
+            AllProduct allproduct = new AllProduct();
+            Product product = new Product();
+            product.SetName("Test1");
+            product.SetContent("Very Expensive");
+            product.SetPrice(300000);
+            Product product2 = new Product();
+            product2.SetName("Test2");
+            product2.SetContent("Very cheap");
+            product2.SetPrice(10);
+            allproduct.InsertProduct(product);
+            allproduct.InsertProduct(product2);
+            Assert.That("Test2", Is.EqualTo(allproduct.GetProduct(1).getName()));
+        }
+        [Test]
+        public void TestInsertAndRemove()
+        {
+            AllProduct allproduct = new AllProduct();
+            Product product = new Product();
+            product.SetName("Test1");
+            product.SetContent("Very Expensive");
+            product.SetPrice(300000);
+            Product product2 = new Product();
+            product2.SetName("Test2");
+            product2.SetContent("Very cheap");
+            product2.SetPrice(10);
+            allproduct.InsertProduct(product);
+            allproduct.InsertProduct(product2);
+            allproduct.RemoveProduct(product);
+            Assert.That("Test2", Is.EqualTo(allproduct.GetProduct(0).getName()));
+        }
+        [Test]
+        public void TestGetIndex()
+        {
+            AllProduct allproduct = new AllProduct();
+            Product product = new Product();
+            product.SetName("Test1");
+            product.SetContent("Very Expensive");
+            product.SetPrice(300000);
+            Product product2 = new Product();
+            product2.SetName("Test2");
+            product2.SetContent("Very cheap");
+            product2.SetPrice(10);
+            allproduct.InsertProduct(product);
+            allproduct.InsertProduct(product2);
+            Assert.That(1, Is.EqualTo(allproduct.GetProductIndex(product2)));
+        }
     }
 }
