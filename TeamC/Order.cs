b434584988153c5f67fcce49014f66b017ed1193
Chuanfu_Wang <sai8377@yahoo.com.tw>
2016-06-04 00:10:30 +0800

    order && order test

--- TeamC/Order.cs
+++ TeamC/Order.cs
@@ -8,5 +8,46 @@ namespace TeamC
 {
     class Order
     {
+        List<Product> productlist = new List<Product>();
+
+        private string buytime = "";
+        private string paymethod = "";
+        private string orderstate = "";
+
+        public void SetPaymethod(string method)
+        {
+            paymethod = method;
+        }
+        public void SetOrderState(string state)
+        {
+            orderstate = state;
+        }
+
+        public void SetBuyTime(string time)
+        {
+            buytime = time;
+        }
+
+        public string GetOrderState()
+        {
+            return orderstate;
+        }
+
+        //////////////////////////////////////////////////////
+
+        public Product Getproductlist(int indext)
+        {
+            return productlist[indext];
+        }
+
+        public string GetBuyTime()
+        {
+            return buytime;
+        }
+
+        public string GetPayMethod()
+        {
+            return paymethod;
+        }
     }
 }
--- TeamC/OrderTest.cs
+++ TeamC/OrderTest.cs
@@ -10,5 +10,48 @@ namespace TeamC
     [TestFixture]
     class OrderTest
     {
+        [Test]
+        public void Test_SetPaymethod(string input_order_method)
+        {
+            Order order = new Order();
+
+            input_order_method = "貨到付款";
+
+            order.SetPaymethod(input_order_method);
+
+            string output = order.GetPayMethod();
+
+            Assert.That(output, Is.EqualTo(input_order_method));
+
+        }
+
+        [Test]
+        public void Test_SetOrderState(string input_order_state)
+        {
+            Order order = new Order();
+
+            input_order_state = "出貨";
+
+            order.SetOrderState(input_order_state);
+
+            string output = order.GetOrderState();
+
+            Assert.That(output, Is.EqualTo(input_order_state));
+        }
+
+
+        [Test]
+        public void Test_SetBuytime(string input_order_buytime)
+        {
+            Order order = new Order();
+
+            input_order_buytime = "12:30";
+
+            string output = order.GetBuyTime();
+
+            Assert.That(output, Is.EqualTo(input_order_buytime));
+        }
+
+
     }
 }
