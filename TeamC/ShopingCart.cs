aae4fb29e01b2c61830b31c5c1039d9b4249d6cb
huangyijie <738515079@qq.com>
2016-06-03 17:14:17 +0800

    ShopingCart finish

--- TeamC/ShopingCart.cs
+++ TeamC/ShopingCart.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace TeamC
 {
+
     class ShopingCart
     {
+        List<Product> productlist = new List<Product>();
+        public void InsertProduct(Product product)
+        {
+            productlist.Add(product);
+        }
+        public string showName(int index)
+        {
+            string x = productlist[index].getName();
+            return x;
+        }
+        public string showContent(int index)
+        {
+            string x = productlist[index].getContent();
+            return x;
+        }
+        public int showPrice(int index)
+        {
+            int x = productlist[index].getPrice();
+            return x;
+        }
+        public Product GetProduct(int index)
+        {
+            return productlist[index];
+        }
     }
 }
--- TeamC/ShopingCartTest.cs
+++ TeamC/ShopingCartTest.cs
@@ -10,5 +10,39 @@ namespace TeamC
     [TestFixture]
     class ShopingCartTest
     {
+
+        [Test]
+        public void TestInsertProduct()
+        {
+            Product product = new Product();
+            ShopingCart productlist = new ShopingCart();
+            product.SetName("name");
+            product.SetContent("content");
+            product.SetPrice(100);
+            productlist.InsertProduct(product);
+            Assert.That("name", Is.EqualTo(productlist.showName(0)));
+            Assert.That("content", Is.EqualTo(productlist.showContent(0)));
+            Assert.That(100, Is.EqualTo(productlist.showPrice(0)));
+        }
+        [Test]
+        public void TestGetProduct()
+        {
+            Product product = new Product();
+            Product product2 = new Product();
+            ShopingCart productlist = new ShopingCart();
+            product.SetName("name1");
+            product.SetContent("content");
+            product.SetPrice(100);
+            productlist.InsertProduct(product);
+            product2.SetName("name2");
+            product2.SetContent("content2");
+            product2.SetPrice(200);
+            productlist.InsertProduct(product2);
+            Assert.That("name1", Is.EqualTo(productlist.GetProduct(0).getName()));
+            Assert.That("name2", Is.EqualTo(productlist.GetProduct(1).getName()));
+            Assert.That(100, Is.EqualTo(productlist.GetProduct(0).getPrice()));
+            Assert.That(200, Is.EqualTo(productlist.GetProduct(1).getPrice()));
+        }
     }
 }
+
