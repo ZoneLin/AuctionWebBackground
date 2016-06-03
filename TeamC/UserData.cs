456301ff0125be69ffa8a72c6cc90b4d2cc47df3
Evil_Evince <821204616@qq.com>
2016-06-03 14:10:17 +0800

    UserData

--- TeamC/UserData.cs
+++ TeamC/UserData.cs
@@ -8,5 +8,51 @@ namespace TeamC
 {
     class UserData
     {
+        private string password;
+        private int id;
+        private int usertype;
+        private List<Order> orderlist=new List<Order>();
+        private List<string> payinformation;
+        private string account;
+        public  UserData(int uid=0,string pwd="")
+        {
+            id = uid;
+            password = pwd;
+            account = "";
+            usertype  = 0;
+        }
+        public void SetPayinformation(List<string> Payinformation)
+        {
+            payinformation = Payinformation;
+        }
+        public List<string> GetPayinformation()
+        {
+            return payinformation;
+        }
+        public void SetPassword(string pwd)
+        {
+            password = pwd;
+        }
+        public string GetPassword()
+        {
+            return password;
+        }
+        public void SetUserType(int UserType)
+        {
+            usertype = UserType;
+        }
+        public int GetUserType()
+        {
+            return usertype;
+        }
+        public void SetOrderList(Order order)
+        {
+            orderlist.Add(order);
+        }
+        public Order GetOrderList()
+        {
+            return orderlist[0];
+        }
+
     }
 }
--- TeamC/UserDataTest.cs
+++ TeamC/UserDataTest.cs
@@ -10,5 +10,42 @@ namespace TeamC
     [TestFixture]
     class UserDataTest
     {
+        [Test]
+        public void TestSetandGetPayinformation()
+        {
+            UserData user = new UserData();
+            List<string> Payinformation=new List<string>();
+            Payinformation.Add("12345678");
+            user.SetPayinformation(Payinformation);
+            Assert.That(Payinformation, Is.EqualTo(user.GetPayinformation()));
+
+        }
+        [Test]
+        public void TestSetandGetPassword()
+        {
+            UserData user = new UserData();
+            string passwd = "12345678";
+            user.SetPassword(passwd);
+            Assert.That(passwd, Is.EqualTo(user.GetPassword()));
+
+        }
+        [Test]
+        public void TestSetandGetUsertype()
+        {
+            UserData user = new UserData();
+            int UserType = 1;
+            user.SetUserType(UserType);
+            Assert.That(UserType, Is.EqualTo(user.GetUserType()));
+
+        }
+        [Test]
+        public void TestSetGetOrderList()
+        {
+            UserData user = new UserData();
+            Order ord=new Order();
+            user.SetOrderList(ord);
+            Assert.That(ord, Is.EqualTo(user.GetOrderList()));
+
+        }
     }
 }
