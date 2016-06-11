c705f6bf9f5e9b8a376188550bfb891e3a60e665
Evil_Evince <821204616@qq.com>
2016-06-11 19:09:17 +0800

    添加 ShoppingSystem & test

---
+++ TeamC/ShoppingSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamC
+{
+    class ShoppingSystem
+    {
+        private Order order=new Order();
+        private AllProduct allproduct=new AllProduct();
+        private User user =new User();
+
+        public void Registered(string id, string password)
+        {
+            user.Registered(id,password);
+        }
+        public Boolean Login(string id, string password)
+        {
+            Boolean flag=user.Login(id, password);
+            return flag;
+        }
+        public void Logout()
+        {
+            user.Logout();
+        }
+        public UserData GetUserData(string id)
+        {
+            UserData userdata = user.GetUserData(id);
+            return userdata;
+        }
+        public UserData GetUserData(int index)
+        {
+            UserData userdata = user.GetUserData(index);
+            return userdata;
+        }
+        public UserData  GetCurrentLoginUser(string id)
+        {
+            UserData userdata = user.GetUserData(id);
+            return userdata;
+        }
+
+
+
+
+    }
+}
---
+++ TeamC/ShoppingSystemTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamC
+{
+    [TestFixture]
+    class ShoppingSystemTest
+    {
+        [Test]
+        public void TestCreateNewAccount()
+        {
+            ShoppingSystem users = new ShoppingSystem();
+            users.Registered("A", "pswA");
+            Assert.That(users.GetUserData("A").GetAccount(), Is.EqualTo("A"));
+            Assert.That(users.GetUserData("A").GetPassword(), Is.EqualTo("pswA"));
+        }
+        [Test]
+        public void TestGetUserData()
+        {
+            ShoppingSystem users = new ShoppingSystem();
+            users.Registered("A", "pswA");
+            // GetUserData(int)
+            Assert.That(users.GetUserData(0).GetAccount(), Is.EqualTo("A"));
+            Assert.That(users.GetUserData(0).GetPassword(), Is.EqualTo("pswA"));
+            Assert.That(users.GetUserData(1), Is.EqualTo(null));
+            // GetUserData(string)
+            Assert.That(users.GetUserData("A").GetAccount(), Is.EqualTo("A"));
+            Assert.That(users.GetUserData("A").GetPassword(), Is.EqualTo("pswA"));
+            Assert.That(users.GetUserData("B"), Is.EqualTo(null));
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            ShoppingSystem users = new ShoppingSystem();
+            users.Registered("A", "pswA");
+
+            Assert.That(users.Login("A", "pswA"), Is.EqualTo(true));
+            Assert.That(users.Login("A", "BBBBB"), Is.EqualTo(false));
+            Assert.That(users.Login("B", "BBBBB"), Is.EqualTo(false));
+        }
+    }
+}
