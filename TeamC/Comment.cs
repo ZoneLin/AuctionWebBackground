37e3f424aa9265a4b32d0aec3690e22c20ee31ca
tim84312 <q8952526@gmail.com>
2016-06-17 22:52:30 +0800

    評論class的Set & Get

---
+++ TeamC/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamC
+{
+    class Comment
+    {
+        private string comment;
+        public Comment(string comment_input)
+        {
+            comment = comment_input;
+        }
+        public Comment()
+        {
+            comment = "";
+        }
+        public void Setcomment(string comment_input)
+        {
+            comment = comment_input;
+        }
+        public string Getcomment()
+        {
+            return comment;
+        }
+    }
+}
---
+++ TeamC/CommentTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace TeamC
+{
+    [TestFixture]
+    class CommentTest
+    {
+        [Test]
+        public void Test_initAndGetcomment()
+        {
+            Comment comment = new Comment("nice");
+            Assert.That(comment.Getcomment(), Is.EqualTo("nice"));
+        }
+        [Test]
+        public void Test_SetAndGetcomment()
+        {
+            Comment comment = new Comment("nice");
+            comment.Setcomment("bad");
+            Assert.That(comment.Getcomment(), Is.EqualTo("bad"));
+        }
+    }
+}
